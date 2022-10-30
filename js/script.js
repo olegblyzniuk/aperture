d3891cd18ce393482043b6b28e7694884075b306
olegblyzniuk <mettal062@gmail.com>
2022-10-30 16:29:30 +0300

    owl carousel and section clients

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $( function() {
     $( "#accordion" ).accordion();
   } );
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', '' ],
+    autoplay: true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
